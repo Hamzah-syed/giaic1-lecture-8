82e4e0d16c383915e55e989209cbdc80baf28532
hamzah syed <hamzah.syed17@gmail.com>
2024-10-25 11:37:08 +0500

    added hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div className="">{`What's your name`}</div>;
+  return (
+    <div className="">
+      <h1>Hello World</h1>
+    </div>
+  );
 }
